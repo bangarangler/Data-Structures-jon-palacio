fb608e23c066fb3a745422a0243ea80784c2299a
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-06-10 15:27:04 -0400

    Linked List Queue

--- queue/queue.py
+++ queue/queue.py
@@ -3,13 +3,19 @@ class Queue:
     self.size = 0
     # what data structure should we
     # use to store queue elements?
-    self.storage = 
+    # linked list
+    self.storage = []
 
   def enqueue(self, item):
-    pass
-  
+    self.storage.insert(0, item)
+    self.size += 1
+
   def dequeue(self):
-    pass
+    if self.size > 0:
+      self.size -= 1
+      return self.storage.pop()
+    else:
+      return None
 
   def len(self):
-    pass
+    return self.size
